7d43bb4455110835a2b871364a21d295bcd46ba7
franciscc <49594803+franciscc@users.noreply.github.com>
2023-03-16 20:34:26 -0300

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    SidebarComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
